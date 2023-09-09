249347411e97a875b8f72ffcca61dfc711458935
alexjruc <justworks2021@gmail.com>
2023-09-09 14:04:17 -0500

    add filter

--- main.js
+++ main.js
@@ -12,25 +12,58 @@ async function getProducts() {
 }
 
 function printProducts(arr) {
-	const products = document.querySelector(".products")
+	const products = document.querySelector(".products");
 
 	htmlAdd = "";
 	for (const product of arr) {
-		htmlAdd += `<div class="product">
-						<img class="product_img" src="${product.image}" alt="${product.category}">
-						<div class="product_info">
-							<h3>$${product.price}.00 <span>Stock: ${product.quantity}</span></h3>
-							<p>${product.name}</p>
-						</div>
-					</div>`
+			htmlAdd += `<div class="product">
+							<img class="product_img" src="${product.image}" alt="${product.category}">
+							<div class="product_info">
+								<h3>$${product.price}.00 <span>Stock: ${product.quantity}</span></h3>
+								<p>${product.name}</p>
+							</div>
+						</div>`
+		
 	}
 	products.innerHTML = htmlAdd
 }
 
+function printFilterProducts(allProducts, category) {
+	let arr = [];
+		for (const product of allProducts) {
+			if(product.category === category){
+				arr.push(product);
+				printProducts(arr)
+				}
+			}
+}
+
+function handleFilter(allProducts){
+		
+	const filter_products = document.querySelector(".filter_products")
+	filter_products.addEventListener("click", function(e){
+		if(e.target.closest(".filter_all")) {
+			printProducts(allProducts);
+		}
+		if(e.target.closest(".filter_shirt")) {
+			printFilterProducts(allProducts, "shirt")
+		}
+		if(e.target.closest(".filter_hoddie")) {
+			printFilterProducts(allProducts, "hoddie")
+		}
+		if(e.target.closest(".filter_sweater")) {
+			printFilterProducts(allProducts, "sweater")
+		}
+		
+	})
+}
+
+
 async function main() {
-	const merchandise = await getProducts();
-	console.log(merchandise);
-	printProducts(merchandise);
+	const allProducts = await getProducts();
+	console.log(allProducts);
+	printProducts(allProducts);
+	handleFilter(allProducts);
 }
 
 
